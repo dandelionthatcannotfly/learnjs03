120281d3eb8ee87c97b8974acb4793011a852832
dandelionthatcannotfly <sjhp2050@163.com>
2023-07-19 10:48:51 +0800

    scope

---
+++ scope/priority.js
@@ -0,0 +1,6 @@
+//优先级
+//变量覆盖
+var x=1;
+function x(){}
+
+console.log(typeof x); //number
---
+++ scope/static-scopt.js
@@ -0,0 +1,28 @@
+//作用域
+//1.静态作用域，（词法作用域）
+//2.动态作用域
+//JS内部没有动态作用域
+
+//所有定义的变量在binding阶段，按照词法作用域进行
+//动态作用域，变量的binding，在执行阶段才进行
+let x=10
+function test(){
+    console.log(x)
+}
+
+test()
+
+function main(){
+    let x=20
+    test()
+}
+
+main()
+
+
+//静态作用域binding的时变量，和变量的值无关
+//变量的值需要在调用时才能知道是多少
+/** 
+ * const env={x_global:10}
+ * const identifier={test}
+*/
