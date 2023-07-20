aa1612d554cc259a62d14902d0d2ea8688d74613
dandelionthatcannotfly <sjhp2050@163.com>
2023-07-20 10:27:57 +0800

    树

---
+++ data-structure/tree.js
@@ -0,0 +1,27 @@
+const treeNode2={
+    val:1,
+    left:null,
+    right:null
+}
+
+const treeNoode1={
+    val:2,
+    left:null,
+    right:null
+}
+const treeRoot={
+    val:3,
+    left:treeNoode1,
+    right:treeNode2
+}
+
+
+treeRoot.right;
+treeRoot.left;
+
+
+//树的遍历
+//前序遍历
+//中序遍历--二叉搜索树中序遍历是递增数列
+//后序遍历
+//层序遍历
